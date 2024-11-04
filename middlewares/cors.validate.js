cf4ec1a83ff458d2445018bfa8855d756469fc3b
root <pcabreram@edenorte.com.do>
2024-11-03 22:39:57 -0400

    midlewarese folder removed

--- middlewares/cors.validate.js
+++
@@ -1,8 +0,0 @@
-const cors = require("cors");
-require("dotenv").config();
-
-cors({
-  allowedHeaders: process.env.CORS_ORIGIN_ALLOWED,
-  origin: process.env.CORS_ORIGIN_ALLOWED,
-  methods: ["GET", "POST"],
-});
