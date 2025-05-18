7dc03e822636d5c7451049340add2ae4909d875d
Phillip Leonardo Cabrera Medrano <pcabreram@edenorte.com.do>
2025-05-17 23:05:48 -0400

    Addinn async word in the sendErrorMessages Function to correctly manage the start, waiting and end of sened messages

--- handler.js
+++ handler.js
@@ -1,23 +1,22 @@
 const { sendPendingMessages } = require("./jobs/sendPendingMessages");
 const { sendErrorMessages } = require("./jobs/handleErrorMessges");
 
-
-
-
 exports.handler = async (event) => {
-    console.log("Evento recibido:", JSON.stringify(event, null, 2));
-    try {
-        console.log("Iniciando tarea: Enviar mensajes pendientes...");
-        await sendPendingMessages().then(() => {
-            console.log("Finalizada tarea: Enviar mensajes pendientes...");
-        }).then(() => {
-            console.log("Iniciando tarea: Manejar mensajes con error...");
-            sendErrorMessages().then(() => {
-                console.log("Finalizada tarea: Manejar mensajes con error...");
-            })
+  console.log("Evento recibido:", JSON.stringify(event, null, 2));
+  try {
+    console.log("Iniciando tarea: Enviar mensajes pendientes...");
+    await sendPendingMessages()
+      .then(() => {
+        console.log("Finalizada tarea: Enviar mensajes pendientes...");
+      })
+      .then(async () => {
+        console.log("Iniciando tarea: Manejar mensajes con error...");
+        await sendErrorMessages().then(() => {
+          console.log("Finalizada tarea: Manejar mensajes con error...");
         });
-    } catch (error) {
-        console.error("Error ejecutando las tareas:", error);
-        throw new Error("Error en las tareas programadas");
-    }
+      });
+  } catch (error) {
+    console.error("Error ejecutando las tareas:", error);
+    throw new Error("Error en las tareas programadas");
+  }
 };
